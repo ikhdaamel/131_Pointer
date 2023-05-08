6bfc4820e3790ee5061880838e5971505bb4e720
ikhdaamel <ikhda.new45@gmail.com>
2023-05-08 11:18:37 +0700

    membuat dereference dengan menggunakan new

--- pointer2/pointer2.cpp
+++ pointer2/pointer2.cpp
@@ -8,3 +8,11 @@ class mahasiswa {
 		cout << "no induk = " << nim << endl;
 	}
 };
+
+int main() {
+	mahasiswa* mhs = new mahasiswa{ 1 };
+	mhs->nim = 2;
+	mhs->showNim();
+	delete mhs;
+return 0;
+}
